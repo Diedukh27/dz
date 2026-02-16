7db8e8324235b0d5453ccbf735d11ed2e40b04b5
coder <coder@SCH-A42PC14>
2026-02-16 13:18:19 +0200

    16.02

--- WebATB/WebATB/Data/Entities/CategoryEntity.cs
+++ WebATB/WebATB/Data/Entities/CategoryEntity.cs
@@ -12,6 +12,6 @@ public class CategoryEntity
     public string Name { get; set; } = null!;   // Обов’язкове поле, максимум 250 символів
     [Required, StringLength(250)]
     public string Slug { get; set; } = null!;  
-    [StringLength(100)]       // Необов’язкове поле, максимум 100 символів
+    [StringLength(250)]       // Необов’язкове поле, максимум 100 символів
     public string? Image { get; set; }
 }
